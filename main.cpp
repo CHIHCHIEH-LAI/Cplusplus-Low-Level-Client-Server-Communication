0c9c4524fbac45e82d397122c088fd64c1478848
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-13 15:28:14 +0800

    refactor: Update main.cpp to support multi-threading for server and load balancer

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
 #include <vector>
+#include <thread>
 
 #include "load_balancer.h"
 #include "load_balancer_strategy.h"
 #include "round_robin_strategy.h"
 #include "server.h"
 
-int main() {
-    std::vector<int> server_ports = {8080, 8081, 8082};
+void run_server(int port) {
+    Server server(port);
+    server.run(5);
+}
+
+void run_balancer(int port, std::vector<int> &server_ports) {
 
     // Choose the load balancing strategy
     std::unique_ptr<LoadBalancerStrategy> strategy = std::make_unique<RoundRobinStrategy>();
 
-    LoadBalancer load_balancer(8080, server_ports, std::move(strategy));
-    load_balancer.run();
-    return 0;
+    LoadBalancer load_balancer(port, server_ports, std::move(strategy));
+    load_balancer.run(5);
+}
+
+int main() {
+    std::vector<int> server_ports = {8081, 8082, 8083, 8084, 8085};
+    std::vector<std::thread> server_threads;
+
+    for (int port : server_ports) {
+        server_threads.push_back(std::thread(run_server, port));
+    }
+
+    std::thread balancer_thread(run_balancer, 8080, std::ref(server_ports));
+
+    for (std::thread &thread : server_threads) {
+        thread.join();
+    }
+
+    balancer_thread.join();
 }
