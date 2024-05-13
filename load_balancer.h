cdf94256edad9e4738b091c1c7cbf9bed0c1be97
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-13 12:03:34 +0800

    feat: Add LoadBalancer class for load balancing server ports

---
+++ load_balancer.h
@@ -0,0 +1,27 @@
+#ifndef LOADBALANCER_H
+#define LOADBALANCER_H
+
+#include <vector>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <memory>
+#include <iostream>
+
+#include "load_balancing_strategy.h"
+
+class LoadBalancer {
+public:
+    LoadBalancer(int port, const std::vector<int>& server_ports, std::unique_ptr<LoadBalancingStrategy> strategy);
+    void run();
+
+private:
+    int balancer_socket;
+    std::vector<int> server_ports;
+    std::unique_ptr<LoadBalancingStrategy> strategy;
+
+    void handle_client(int client_socket);
+    int get_server_socket();
+};
+
+#endif // LOADBALANCER_H
